824a79825888d6eb1a2203f5bba3f51357bcc301
Bill Patrianakos <bill@linus>
2015-06-05 09:35:20 -0500

    Add QClient function and object prototype.

--- qclient.js
+++ qclient.js
@@ -4,6 +4,10 @@
  * Main module exported from this library.
  */
 
+// Require our dependencies
+var http    = require('http'),
+    crypto  = require('crypto');
+
 /**
  * QClient
  * -------
@@ -15,5 +19,63 @@
  * `secretToken` [String] - The secret token you are given when signin up for Q
  */
 function QClient(clientId, secretToken) {
-  // body...
+  if (!(this instanceof QClient)) return new QClient(clientId, secretToken);
+
+  this.setApiKey(clientId);
+  this.setToken(secretToken);
 }
+
+QClient.prototype = {
+  setApiKey: function(key) {
+    if (key) this.clientId = key;
+  },
+
+  setToken: function(secret) {
+    if (secret) this.secretToken = secret;
+  },
+
+  /**
+   * Signature
+   * ---------
+   * Build a signature to send in the 
+   * request header.
+   */
+  signature: function(endpoint, reqBody) {
+    var hmac = crypto.createHmac('sha1', this.secretToken).setEncoding('hex');
+    hmac.write(endpoint + reqBody);
+    hmac.end();
+    return hmac.read();
+  },
+
+  getSubsidy: function(state, zip, demographics) {
+    var endpoint      = ['/plans', state, zip].join('/'),
+        demographics  = JSON.stringify(demographics);
+
+    var request = http.request({
+      host: 'localhost',
+      path: endpoint,
+      port: 9292,
+      method: 'POST',
+      headers: { 'Authorization': [this.clientId, this.signature(endpoint, demographics)].join(':') }
+    }, function(response) {
+      var reply = '';
+      response.on('data', function(chunk) {
+        reply += chunk;
+      });
+
+      response.on('end', function() {
+        console.log(reply);
+
+        return reply;
+      });
+    });
+
+    request.write(demographics);
+
+    console.log(endpoint, [this.clientId, this.signature(endpoint, demographics)].join(':'));
+
+    return request.end();
+  }
+};
+
+module.exports = QClient;
