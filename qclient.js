652e24638362d2fc099d2dcef91465bdc1dba38a
Bill Patrianakos <bill@linus>
2015-06-05 10:43:42 -0500

    Fix issue where requests to API threw 400 errors.
    In newer versions of Node we need to use the `https` module to make requests
    over SSL. Replaced our http module calls with https versions.

--- qclient.js
+++ qclient.js
@@ -5,7 +5,7 @@
  */
 
 // Require our dependencies
-var http    = require('http'),
+var https    = require('https'),
     crypto  = require('crypto');
 
 /**
@@ -17,19 +17,27 @@ var http    = require('http'),
  *
  * `clientId` [String] - The Client ID you are given when signing up for Q
  * `secretToken` [String] - The secret token you are given when signin up for Q
+ * `options` [Object] - Options object. DO NOT USE THIS. For development only.
  */
-function QClient(clientId, secretToken) {
+function QClient(clientId, secretToken, options) {
   if (!(this instanceof QClient)) return new QClient(clientId, secretToken);
+  var opts = (typeof options === 'undefined') ? {} : options;
+
+  // Allow overriding options. Do not use this outside of development.
+  this.host = (typeof opts.host === 'undefined') ? 'q.aploquote.com' : opts.host;
+  this.port = (typeof opts.port === 'undefined') ? 443 : opts.host;
 
   this.setApiKey(clientId);
   this.setToken(secretToken);
 }
 
 QClient.prototype = {
+  // API key setter method
   setApiKey: function(key) {
     if (key) this.clientId = key;
   },
 
+  // Secret token setter
   setToken: function(secret) {
     if (secret) this.secretToken = secret;
   },
@@ -59,19 +67,21 @@ QClient.prototype = {
    * `zip` [String, Number] - 5 digit zip code
    * `demographics` [Object] - Object containing rating information. See
    *    Q API docs for more on the structure of this object.
-   * `cb` [Function] - Function called when response is available
+   * `cb` [Function] - Function called when response is available. Should take a 
+   *    single String for arguments.
    */
   getSubsidy: function(state, zip, demographics, cb) {
     var endpoint      = ['/plans', state, zip].join('/');
         demographics  = JSON.stringify(demographics);
 
-    var request = http.request({
-      host: 'localhost',
+    var request = https.request({
+      host: this.host,
       path: endpoint,
-      port: 9292,
+      port: this.port,
       method: 'POST',
       headers: {
         'Authorization': this.clientId + ':' + this.signature(endpoint, demographics),
+        'Content-Type': 'application/json',
         'Content-Length': demographics.length
       }
     }, function(response) {
